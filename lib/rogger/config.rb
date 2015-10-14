88e5f524e3acf85904a30290bc37697985ae5407
Lau Siaw Young <lausiawyoung@gmail.com>
2015-10-14 20:49:50 +0800

    Remove deprecated comments

--- lib/rogger/config.rb
+++ lib/rogger/config.rb
@@ -9,7 +9,7 @@ module Rogger
 
       include Errors
       include Utils
-      attr_reader :protocol, :server_hostname, :server_port, :app_name, :disabled #, :app_logging, :log_to_file
+      attr_reader :protocol, :server_hostname, :server_port, :app_name, :disabled
 
       def initialize(file_location)
         @config_file = YAML.load(ERB.new(File.read(file_location)).result)
@@ -23,8 +23,6 @@ module Rogger
         @server_hostname   ||= set_server_hostname
         @server_port       ||= set_server_port
         @app_name          ||= set_app_name
-        # @app_logging       ||= set_app_logging
-        # @log_to_file       ||= set_log_to_file
       end
 
       private
@@ -48,7 +46,6 @@ module Rogger
         end
       end
 
-      # TODO: Can check for valid port numbers
       def set_server_port
         case @env_hash['port']
           when Integer then @env_hash['port']
@@ -62,20 +59,6 @@ module Rogger
           else raise InvalidAppNameError, "Please enter a valid application name"
         end
       end
-
-      # def set_app_logging
-      #   case @env_hash['app_logging']
-      #   when true, false then @env_hash['app_logging']
-      #   else raise InvalidSettingError, "Please set to either true or false for app_logging setting"
-      #   end
-      # end
-
-      # def set_log_to_file
-      #   case @env_hash['log_to_file']
-      #   when true, false then @env_hash['log_to_file']
-      #   else raise InvalidSettingError, "Please set to either true or false for log_to_file setting"
-      #   end
-      # end
     end
 
     def self.env
@@ -83,14 +66,6 @@ module Rogger
       return Sinatra::Base.environment.to_s if defined?(Sinatra)
       ENV["RACK_ENV"] || raise(Errors::NoEnvironment.new)
     end
-
-    # def self.logger
-    #   @@logger
-    # end
-
-    # def self.notifier
-    #   @@notifier
-    # end
 
     config = Config::ConfigurationObject.new('config/rogger.yml')
 
@@ -105,8 +80,6 @@ module Rogger
           environment: env,
           protocol: config.protocol 
         })
-
-      @@notifier = GELF::Notifier.new(config.server_hostname, config.server_port)
 
       Rails.logger.extend(ActiveSupport::Logger.broadcast(@@logger))
         
